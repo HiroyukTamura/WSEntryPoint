45d6d66038601a026f5346c30e36ad826533e94d
hiroyuki <hiroyukitamura@gmail.com>
2018-01-07 05:28:15 +0900

    ソーシャル画面作ってるよね

---
+++ social/app.js
@@ -0,0 +1,61 @@
+const postLoad = $('#post_load');
+var progress = $('#progress');
+
+function init() {
+    var config = {
+        apiKey: "AIzaSyBQnxP9d4R40iogM5CP0_HVbULRxoD2_JM",
+        authDomain: "wordsupport3.firebaseapp.com",
+        databaseURL: "https://wordsupport3.firebaseio.com",
+        projectId: "wordsupport3",
+        storageBucket: "wordsupport3.appspot.com",
+        messagingSenderId: "60633268871"
+    };
+    defaultApp = firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log("ユーザログインしてます");
+            // progress.css("display", "none");
+            onLoginSuccess(user.uid);
+        } else {
+            firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+                .then(function() {
+                    var uiConfig = {
+                        signInOptions: [
+                            // Leave the lines as is for the providers you want to offer your users.
+                            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                            firebase.auth.PhoneAuthProvider.PROVIDER_ID
+                        ],
+                        // Terms of service url.
+                        tosUrl: 'sampleTosUrl',
+                        'callbacks': {
+                            // Called when the user has been successfully signed in.
+                            'signInSuccess': function(user, credential, redirectUrl) {
+                                console.log(user.uid);
+                                progress.css('display', "none");
+                                $('#login_w').css('display', "none");
+                                onLoginSuccess(user.uid);
+                                return false;
+                            }
+                        }
+                    };
+
+                    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+                    progress.css('display', "none");
+                    $('#login_w').css('display', "inline");
+                    ui.start('#firebaseui-auth-container', uiConfig);
+                }).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+                //todo エラー時の処理
+            });
+        }
+    });
+}
+
+function onLoginSuccess(uid) {
+    console.log("onLoginSuccess:", uid);
+}
