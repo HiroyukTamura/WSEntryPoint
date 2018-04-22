904842dc1fe90791e8238fec4859ecb6ec14d9de
hiroyuki <hiroyukitamura@gmail.com>
2018-04-22 09:54:12 +0900

    4/22 作業開始

--- public/analytics/app2.js
+++ public/analytics/app2.js
@@ -1,53 +1,65 @@
 "use strict";
 
-const postLoad = $('#post_load');
-const progress = $('#progress');
-const loginUi = $('#login_w');
 const mainContent = $('.page-content');
-let defaultDatabase;
-let loginedUser;
 
 window.onload = function init() {
-    const defaultApp = firebase.initializeApp(CONFIG);
-    defaultDatabase = defaultApp.database();
-
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            console.log("ユーザログインしてます");
-            loginedUser = user;
-            onLoginSuccess();
-        } else {
-            firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-                .then(function() {
-                    const uiConfig = createFbUiConfig(function (user, credential, redirectUrl) {
-                        progress.hide();
-                        loginUi.hide();
-                        loginedUser = user;
-                        onLoginSuccess();
-                        return false;
-                    });
-
-                    progress.hide();
-                    postLoad.hide();
-                    const ui = new firebaseui.auth.AuthUI(firebase.auth());
-                    $('#login_w').show();
-                    ui.start('#firebaseui-auth-container', uiConfig);
-
-                }).catch(function(error) {
-                console.log(error.code, error.message);
-                showOpeErrNotification(defaultDatabase);
-            });
-        }
-    });
-
-    initDrawerDecoration();
+    new Main().init();
 };
 
-function onLoginSuccess() {
-    console.log("onLoginSuccess");
-    progress.hide();
-    loginUi.hide();
-    postLoad.show();
+class Main {
 
-    setDrawerProfile(loginedUser);
+    constructor(){
+        this.postLoad = $('#post_load');
+        this.progress = $('#progress');
+        this.loginUi = $('#login_w');
+        this.mainContent = $('.page-content');
+        // this.defaultDatabase;
+        this.loginedUser;
+    }
+
+    init(){
+        const defaultApp = firebase.initializeApp(CONFIG);
+        const defaultDatabase = defaultApp.database();
+        const self = this;
+
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log("ユーザログインしてます");
+                self.loginedUser = user;
+                self.onLoginSuccess();
+            } else {
+                firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+                    .then(function() {
+                        const uiConfig = createFbUiConfig(function (user, credential, redirectUrl) {
+                            self.progress.hide();
+                            self.loginUi.hide();
+                            self.loginedUser = user;
+                            self.onLoginSuccess();
+                            return false;
+                        });
+
+                        self.progress.hide();
+                        self.postLoad.hide();
+                        const ui = new firebaseui.auth.AuthUI(firebase.auth());
+                        $('#login_w').show();
+                        ui.start('#firebaseui-auth-container', uiConfig);
+
+                    }).catch(function(error) {
+                    console.log(error.code, error.message);
+                    showOpeErrNotification(defaultDatabase);
+                });
+            }
+        });
+
+        initDrawerDecoration();
+    }
+
+    onLoginSuccess() {
+        console.log("onLoginSuccess");
+        this.progress.hide();
+        this.loginUi.hide();
+        this.postLoad.show();
+
+        setDrawerProfile(this.loginedUser);
+    }
 }
